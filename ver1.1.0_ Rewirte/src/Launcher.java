1e1a38b493db4a3e28c9f80ea3fc415fd8460d23
bapnontiensinh <nnd2342001@gmail.com>
2020-09-25 23:16:29 +0700

    Rewirte

---
+++ ver1.1.0_ Rewirte/src/Launcher.java
@@ -0,0 +1,15 @@
+import java.io.FileNotFoundException;
+
+public class Launcher {
+    public static void main(String[] args) throws FileNotFoundException {
+        DictionaryManagement dictionaryManagement = new DictionaryManagement();
+        DictionaryCommandline dictionaryCommandline = new DictionaryCommandline();
+        // ver 1.0
+        // dictionaryCommandline.dictionaryBasic(dictionaryManagement);
+        //ver 1.1
+
+        dictionaryCommandline.dictionaryAdvance(dictionaryManagement);
+        dictionaryManagement.dataModify();
+        dictionaryCommandline.showAllWord(dictionaryManagement.dictionary);
+    }
+}
